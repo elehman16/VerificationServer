ba9880df065bd50c7e342d95a1fb48027c530eb7
Eric Lehman <lehmer16@gmail.com>
2018-05-17 15:38:06 -0400

    Fixing bug

--- agreement_statistics.py
+++ agreement_statistics.py
@@ -182,9 +182,8 @@ def get_stats(art_id):
         data_loc.remove('.//data//for-full-text-annotation.csv')
         data_loc.remove('.//data//prompt_gen.csv')
     except:
-        print(data_loc)
-        data_loc.remove('.//data\for-full-text-annotation.csv')
-        data_loc.remove('.//data\prompt_gen.csv')
+        data_loc.remove('.//data/for-full-text-annotation.csv')
+        data_loc.remove('.//data/prompt_gen.csv')
     """
     data_loc = ['.//data//out_lidija.csv', './/data//out_edin.csv',
                 './/data//out_milorad.csv', './/data//out_sergii.csv',
